f37130cb7c922be503c1db1a5dbf087d2f4547aa
warspite1126 <mail@warspite.navy>
2020-07-31 04:04:27 +0900

    add

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 "use strict";
 
-const textArea = document.getElementById("textArea");
-const words = document.getElementById("words");
-const characters = document.getElementById("characters");
+const textArea = document.querySelector("#textArea");
+const words = document.querySelector("#words");
+const characters = document.querySelector("#characters");
 
-const textColourChanger = document.getElementById("textColourChanger");
+const textColourChanger = document.querySelector("#textColourChanger");
 
-const fontSizeSlider = document.getElementById("fontSizeSlider");
-const fontSizer = document.getElementById("fontSizer")
-
+const fontSizeSlider = document.querySelector("#fontSizeSlider");
+const fontSizer = document.querySelector("#fontSizer");
 
 const count = () => {
   const text = textArea.value;
 
   const textAreaCharacters = text.replace(/\s/g, "");
 
-  const textAreaWords = text.split(/\s+/).filter((e) => e);
+  const textAreaWords = text
+    .split(/\s+/)
+    .filter((e) => e.match(/^[a-zA-Z\d]+$/));
+
+  console.log(textAreaWords);
 
   words.value = textAreaWords.length;
   characters.value = textAreaCharacters.length;
@@ -23,7 +26,7 @@ const count = () => {
 
 const changeTextColour = (e) => {
   textArea.style.color = e.target.value;
-}
+};
 
 const changefontSizer = (e) => {
   textArea.style.fontSize = `${e.target.value}px`;
@@ -33,7 +36,7 @@ const changefontSizer = (e) => {
 
 textArea.oninput = count;
 
-textColourChanger.addEventListener("input", changeTextColour)
+textColourChanger.addEventListener("input", changeTextColour);
 
 fontSizeSlider.addEventListener("input", changefontSizer);
 fontSizer.addEventListener("input", changefontSizer);
